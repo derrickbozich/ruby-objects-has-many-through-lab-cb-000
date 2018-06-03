bb6547347d7ec891bee31af7c6f8071472bcf52a
derrick b <dbozich1@gmail.com>
2018-06-03 01:21:52 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,18 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def new_appointment(date, patient)
+    appointment = Appointment.new(date, patient, self)
+  end
+end
